1baf9b9882bc15a1b10b3bcf391197004601d031
Hannah <filipiniality@protonmail.com>
2026-04-14 20:17:33 -0700

    Renaming CSS files

---
+++ css481-hw/index.js
@@ -0,0 +1,12 @@
+// Based on C
+// No interpretation = you won't see any problems until runtime (that's when interpretation happens)
+/*can do if-else-elif statements*/
+
+const PI=3.14;
+
+console.log("Hello, world!");
+console.log("The value of PI is:", PI);
+
+let y = 20;
+{ y = 21 ;}
+console.log("The value of y is:", y);
