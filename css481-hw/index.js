b70c95522e3fbd5ffe979c9f32985eb7ad07ec55
Hannah <filipiniality@protonmail.com>
2026-04-15 20:50:26 -0700

    Finishing touches

--- css481-hw/index.js
+++ css481-hw/index.js
@@ -2,7 +2,7 @@
 // No interpretation = you won't see any problems until runtime (that's when interpretation happens)
 /*can do if-else-elif statements*/
 
-const PI=3.14;
+/*const PI=3.14;
 
 console.log("Hello, world!");
 console.log("The value of PI is:", PI);
@@ -10,3 +10,53 @@ console.log("The value of PI is:", PI);
 let y = 20;
 { y = 21 ;}
 console.log("The value of y is:", y);
+
+// Create random number array, filter out odd numbers, sort remaining numbers in ascending order.
+
+const myNumbers = [81, 87, 68, 2, 13, 20];
+let indeces = myNumbers.length;
+
+const myOdds = [];
+
+for (let i=0;i<indeces; i++) {
+ if((myNumbers[indeces]) % 2 != 0) {
+        myOdds.push(myNumbers[indeces]);
+    }
+}
+
+for (let i=0;i<indeces;i++) {
+    console.log(myOdds[indeces]);
+}
+
+//
+
+const arr = Array.from()
+const oddArr = [];
+for (const num of arr) {
+    if(num%2)
+}
+
+const sortedOddArr = oddArr.sort((a,b) => a-b );
+
+
+// get all paragraphs and make them red with font-size 20px
+
+const arrParagraphs = document.querySelectorAll("p");
+for (const para of arrParagraphs) {
+    para.style.color = "red";
+    para.style.size = "20px";
+}*/
+
+let axis = new Array([1,2,3,4,5,6,7,8,9,10]);
+let bounds = axis.length;
+
+let xAxis = [];
+let yAxis = [];
+
+for (const num of axis) {
+    for (const num of axis) {
+    }
+}
+
+function printTable(xAxis,yAxis) {
+}
